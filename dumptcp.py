7f3fab9bf9c71f3da555359f43b94b75b5b9202b
ido <ido@IdoHacking.mynet>
2022-10-13 12:04:48 -0400

    changes

--- dumptcp.py
+++ dumptcp.py
@@ -1,10 +1,70 @@
 #!/usr/bin/env python3
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import sniff, IP, Ether, Packet, wrpcap
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from termcolor import colored
 from os import get_terminal_size, system, geteuid
 from time import sleep
+
+types = {
+    "0x800":	"IPv4",
+    "0x806":	"ARP",
+    "0x842":	"Wake-on-LAN",
+    "0x22F0":	"AVTP",
+    "0x22F3":	"IETF TRILL Protocol",
+    "0x22EA":	"Stream Reservation Protocol",
+    "0x6002":	"DEC MOP RC",
+    "0x6003":	"DECnet Phase IV, DNA Routing",
+    "0x6004":	"DEC LAT",
+    "0x8035":	"Reverse Address Resolution Protocol (RARP)",
+    "0x809B":	"AppleTalk (Ethertalk)",
+    "0x80F3":	"AppleTalk Address Resolution Protocol (AARP)",
+    "0x8100":	"VLAN-tagged frame (IEEE 802.1Q) and Shortest Path Bridging IEEE 802.1aq with NNI compatibility[9]",
+    "0x8102":	"Simple Loop Prevention Protocol (SLPP)",
+    "0x8103":	"Virtual Link Aggregation Control Protocol (VLACP)",
+    "0x8137":	"IPX",
+    "0x8204":	"QNX Qnet",
+    "0x86DD":	"IPv6",
+    "0x8808":	"Ethernet flow control",
+    "0x8809":	"LACP",
+    "0x8819":	"CobraNet",
+    "0x8847":	"MPLS unicast",
+    "0x8848":	"MPLS multicast",
+    "0x8863":	"PPPoE Discovery Stage",
+    "0x8864":	"PPPoE Session Stage",
+    "0x887B":	"HomePlug 1.0 MME",
+    "0x888E":	"IEEE 802.1X",
+    "0x8892":	"PROFINET Protocol",
+    "0x889A":	"HyperSCSI (SCSI over Ethernet)",
+    "0x88A2":	"ATA over Ethernet",
+    "0x88A4":	"EtherCAT Protocol",
+    "0x88A8":	"Service VLAN tag identifier (S-Tag) on Q-in-Q tunnel.",
+    "0x88AB":	"Ethernet Powerlink[citation needed]",
+    "0x88B8":	"GOOSE (Generic Object Oriented Substation event)",
+    "0x88B9":	"GSE (Generic Substation Events) Management Services",
+    "0x88BA":	"SV",
+    "0x88BF":	"MikroTik RoMON",
+    "0x88CC":	"LLDP",
+    "0x88CD":	"SERCOS III",
+    "0x88E1":	"HomePlug Green PHY",
+    "0x88E3":	"IEC62439-2",
+    "0x88E5":	"MACsec",
+    "0x88E7":	"PBB",
+    "0x88F7":	"PTP",
+    "0x88F8":	"NC-SI",
+    "0x88FB":	"PRP",
+    "0x8902":	"CFM",
+    "0x8906":	"FCoE",
+    "0x8914":	"FCoE-I",
+    "0x8915":	"RoCE",
+    "0x891D":	"TTE",
+    "0x893a":	"1905.1-IEEE-P",
+    "0x892F":	"HSR",
+    "0x9000":	"Ethernet Configuration Testing Protocol[11]",
+    "0xF1C1":	"Redundancy Tag (IEEE 802.1CB Frame Replication and Elimination for Reliability)"
+}
 
 
 def isSudo():
@@ -29,7 +89,7 @@ class PacketInfo():
     
 
 def feedToPacketInfo(packet:Packet):
-    layer = str(packet.layers()[-1]).split(".")[-1].replace("'>", "")
+    layer = ""
     content = str(packet)
     length = len(content)
     srcMac:str = ''
@@ -39,6 +99,10 @@ def feedToPacketInfo(packet:Packet):
     if packet.haslayer(Ether):
         srcMac = packet[Ether].dst
         dstMac = packet[Ether].src
+        try:
+            layer = types[hex(packet[Ether].type)]
+        except:
+            layer = "Unknown"
         if packet.haslayer(IP):
             srcIp = packet[IP].src
             dstIp = packet[IP].dst
